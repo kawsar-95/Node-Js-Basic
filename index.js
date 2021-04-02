4bc01cb89d60b8a903708d15bf69562d14068747
kawsar-95 <virtualkawsar@gmail.com>
2021-04-02 21:02:43 +0600

    core modules

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('hello world');
-console.log('hello world');
+//path Module
+const path = require('path');
+const myPath = '/home/kawsar/Documents/Node-Js-Basic/index.js';
+
+console.log(path.parse(myPath))
+
+//OS Module
+const os = require('os');
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.freemem())
+console.log(os.cpus())
+//FS Module
