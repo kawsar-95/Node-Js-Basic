26b85aff22d04e509ecccfcaeb5344ec74a919c5
kawsar-95 <virtualkawsar@gmail.com>
2021-04-03 13:09:41 +0600

    core modules

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 //path Module
+
 const path = require('path');
 const myPath = '/home/kawsar/Documents/Node-Js-Basic/index.js';
 
 console.log(path.parse(myPath))
 
 //OS Module
+
 const os = require('os');
 console.log(os.platform());
 console.log(os.homedir());
 console.log(os.freemem())
 console.log(os.cpus())
+
 //FS Module
+const fs = require('fs');
+fs.writeFileSync('myfile.txt','Hello Programmers');
+fs.appendFileSync('myfile.txt',' How are you?');
+
+const data = fs.readFileSync('myfile.txt');
+console.log(data.toString());
