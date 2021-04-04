6c8a91c31bc5e9d6d6959a8b9fd7f9eeabd3837e
kawsar-95 <virtualkawsar@gmail.com>
2021-04-05 01:04:23 +0600

    event Module added

--- index.js
+++ index.js
@@ -19,4 +19,11 @@ fs.writeFileSync('myfile.txt','Hello Programmers');
 fs.appendFileSync('myfile.txt',' How are you?');
 
 const data = fs.readFileSync('myfile.txt');
-console.log(data.toString());
+    console.log(data.toString());
+
+//Asyncrous
+fs.readFile('myfile.txt',(err,data) => {
+    console.log(data.toString());
+});
+
+//Event Module
