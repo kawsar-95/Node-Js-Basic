b78c6c81b2621adfbff6c7f67a4f22201d990775
kawsar-95 <virtualkawsar@gmail.com>
2021-04-09 20:01:57 +0600

    lesson 5 complete

--- lesson5/http.js
+++ lesson5/http.js
@@ -1,1 +1,21 @@
 const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if(req.url ==='/'){
+    res.write('Hello Programmers!');
+    res.write('How are You?');
+    res.end()
+    }
+    else if(req.url === '/about'){
+    res.write('This is about Page!');
+    res.end()
+    }
+    else {
+    res.write('NOT FOUND')
+    res.end()
+    }
+});
+
+
+server.listen(3000);
+console.log('Listening on port 3000 ');
